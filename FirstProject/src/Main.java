27ddf6e5038edcb0503731b4a1ccc0b7d4ae7a9c
Mohammed Najeh Al-zo'ubi <m7md.alzobi10@gmail.com>
2026-03-28 00:07:47 +0300

    Add files via upload

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,30 @@
+public class Main
+{
+
+    public static void main(String[] args)
+      {
+         //comment
+
+         /*
+          System.out.println("Mohammeddddddddddd");
+          System.out.print("Mohammedddddd  \n");
+          System.out.print("Mohammed  \n");
+          */ 
+
+          // int x = 10;
+          // System.out.println(x);
+
+
+          String name = "mohammed";
+          float salary = 5000;
+          boolean employed = true;  
+          int age = 20;
+
+          System.out.println("name :" + name);
+          System.out.println("salary :" + salary);
+          System.out.println("employed :" + employed);
+          System.out.println("age :" + age);
+      }
+
+
+}
